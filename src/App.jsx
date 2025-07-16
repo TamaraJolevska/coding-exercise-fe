afdb9aac9e6264621f4f0773098bfa630a24686b
Tamara Jolevska <tamara.jolevska@gmail.com>
2025-07-16 21:48:50 +0200

    Add table component for showing the matches.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import {TableComponent} from "./components/TableComponent.jsx";
+import {Container} from "@mantine/core";
+import '@mantine/core/styles.layer.css';
 import {useEffect, useState} from 'react'
-import './App.css'
 import {TournamentService} from "./services/TournamentService.js";
 import {MatchService} from "./services/MatchService.js";
 import {SportService} from "./services/SportService.js";
 
 function App() {
 
+    const [sports, setSports] = useState([]);
+    const [selectedSport, setSelectedSport] = useState(null);
+    const [tournaments, setTournaments] = useState([]);
+    const [selectedTournaments, setSelectedTournaments] = useState([]);
+    const [matches, setMatches] = useState([]);
+
     useEffect(() => {
         MatchService.getMatches()
-            .then(data => {
-                console.log('Matches:', data);
-            })
+            .then(setMatches)
             .catch(err => {
                 console.error('Error fetching matches:', err);
             });
@@ -37,7 +43,9 @@ function App() {
     }, []);
 
   return (
-      <div></div>
+          <Container size="md" mt="xl">
+              <TableComponent matches={matches}/>
+          </Container>
   )
 }
 
